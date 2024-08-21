591798fcb0518d2986920fc907af773c07fd4f1a
githinjialbert <githinjialbert001@gmail.com>
2024-08-21 14:34:24 +0300

    the html of the todo app and declaration of the javascript elements

---
+++ JS/todo.js
@@ -0,0 +1,5 @@
+const addNewTaskBtn = document.getElementById("new-task");
+const closeBtn = document.getElementById("close-button");
+const addTaskBtn = document.getElementById("add-task");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");

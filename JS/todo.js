7d9916ef2881e3ebf7494eb1b51f4a2c5e924747
githinjialbert <githinjialbert001@gmail.com>
2024-08-22 12:43:02 +0300

    Updating the js

--- JS/todo.js
+++ JS/todo.js
@@ -1,5 +1,25 @@
-const addNewTaskBtn = document.getElementById("new-task");
-const closeBtn = document.getElementById("close-button");
-const addTaskBtn = document.getElementById("add-task");
+const addNewTaskBtn = document.getElementById("open-task-form-btn");
+const closeTaskBtn = document.getElementById("close-task-form-btn");
+const addTaskBtn = document.getElementById("add-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
+const taskForm = document.getElementById("task-form");
+const inputTaskTitle = document.getElementById("task-title");
+const description = document.getElementById("task-desc");
+const dateInput = document.getElementById("task-date");
+
+const isEmpty = (value) => value === "";
+
+
+
+addNewTaskBtn.addEventListener("click", () => {
+
+    taskForm.style.display = "block";
+    addNewTaskBtn.style.display = "none";
+
+});
+
+closeTaskBtn.addEventListener("click", () => {
+    taskForm.style.display = "none";
+    addNewTaskBtn.style.display = "block";
+});

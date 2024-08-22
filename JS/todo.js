d2771d6ec4dfb70fbe1f96387812b4457ba53772
githinjialbert <githinjialbert001@gmail.com>
2024-08-22 23:52:50 +0300

    an update on the js file. finished the logic side of the close, cancel and discard buttons together with the reset function

--- JS/todo.js
+++ JS/todo.js
@@ -1,13 +1,26 @@
 const addNewTaskBtn = document.getElementById("open-task-form-btn");
+
 const closeTaskBtn = document.getElementById("close-task-form-btn");
+
 const addTaskBtn = document.getElementById("add-task-btn");
+
 const cancelBtn = document.getElementById("cancel-btn");
+
 const discardBtn = document.getElementById("discard-btn");
+
 const taskForm = document.getElementById("task-form");
+
 const inputTaskTitle = document.getElementById("task-title");
+
 const description = document.getElementById("task-desc");
+
 const dateInput = document.getElementById("task-date");
+
 const taskContainer = document.getElementById("task-container");
+
+const confirmCloseDialog = document.getElementById("confirm-close-dialog-btn-container");
+
+
 
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
 const currentTask = {};
@@ -34,6 +47,8 @@ const addOrUpdateTask = () => {
 };
 
 
+
+
 const updateTaskContainer = () => {
     taskContainer.innerHTML = "";
     taskData.forEach(({id, title, date, description}) => {
@@ -50,12 +65,19 @@ const updateTaskContainer = () => {
 };
 
 
+
+
 const deleteTask = (buttonEl) => {
     const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
     buttonEl.parentElement.remove();
     taskData.splice(dataArrIndex, 1);
     localStorage.setItem("data", JSON.stringify(taskData));
 }
+
+
+
+
+
 
 const editTask = () => {
     const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
@@ -72,14 +94,86 @@ const editTask = () => {
 }
 
 
+
+const reset = () => {
+    
+    addTaskBtn.innerText = "Add Task";
+    inputTaskTitle.value = "";
+    dateInput.value = "";
+    description.value = "";
+    taskForm.classList.toggle("hidden");
+    currentTask = {};
+
+}
+
+
+if(taskForm.length) {
+
+    updateTaskContainer();
+
+}
+
 addNewTaskBtn.addEventListener("click", () => {
 
-    taskForm.style.display = "block";
-    addNewTaskBtn.style.display = "none";
+    taskForm.classList.toggle("hidden");
+
 
 });
 
+
+
 closeTaskBtn.addEventListener("click", () => {
-    taskForm.style.display = "none";
-    addNewTaskBtn.style.display = "block";
+
+    const formInputContainsValues = inputTaskTitle.value || dateInput.value || description.value;
+
+    const formValuesUpdated = inputTaskTitle.value !== currentTask.inputTaskTitle || dateInput.value !== currentTask.dateInput
+    || description.value !== currentTask.description;
+
+    if (formInputContainsValues && formValuesUpdated) {
+
+        confirmCloseDialog.showModal();
+        
+    } else {
+
+        reset();
+
+    }
+
+
+    cancelBtn.addEventListener("click", () => {
+
+
+        confirmCloseDialog.close();
+
+
+    })
+
+
+    discardBtn.addEventListener("click", () => {
+
+
+        confirmCloseDialog.close();
+        reset();
+
+
+    })
+
+
+
+
+    taskForm.addEventListener("click", (e) => {
+
+
+        e.preventDefault();
+
+
+
+        addOrUpdateTask();
+
+
+
+    })
+    
+
+
 });
